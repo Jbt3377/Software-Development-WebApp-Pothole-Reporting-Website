720e05f5c95f0727f98be85c343907a60a4e62e8
BEATT <BEATT@LAPTOP-JLEQEJOF>
2019-02-26 00:04:32 +0000

    Updated form for main page and updated report model

--- src/model/Report.java
+++ src/model/Report.java
@@ -3,13 +3,14 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Report implements Serializable{
+	
 	private static final long serialVersionUID = 1L;
 	
+	public String reportID;
 	public String description;
+	public String category;
 	public String details;
-	public ProblemType type;
 	public String filePathToImage;
-	public ArrayList<String> reports = new ArrayList<String>();
 	
 
 }
