8dcf1439f012131c56a5e3e128ede3788ad78032
dingb <dingb@Dells-Dell>
2019-03-22 02:07:06 +0000

    string comparison

--- httpdocs/js/ProfileLoadFunction.js
+++ httpdocs/js/ProfileLoadFunction.js
@@ -1,7 +1,6 @@
-function ProfileLoadFunction(name, email, password, address, filepath ) {
-  document.getElementById("form16").value = name;
-  document.getElementById("form17").value = email;
-  document.getElementById("form18").value = address;
-  document.getElementById("form19").value = password;
-  document.getElementById("form20").value = password;
+function ProfileLoadFunction(name, password, address) {
+  document.getElementById("newName").value = name;
+  document.getElementById("newAddress").value = address;
+  document.getElementById("newPassword").value = password;
+  document.getElementById("newPasswordConfirm").value = password;
 }
